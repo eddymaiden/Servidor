0cb7e31e77618e0de63dde2e874d03ab0c5497da
Miguel Bejar <bejar37@gmail.com>
2011-05-30 14:42:36 +0200

    hello world

--- servidor.c
+++ servidor.c
@@ -8,9 +8,11 @@
 
 void Escribir_Puerto (int puerto);
 
+
 /* FUNCION MAIN DEL PROGRAMA SERVIDOR */
 int main(int argc,char* argv[])
 {
+	printf("HELLO, WORLD!\n");
     /* El esqueleto de la funcion principal muestra, de forma esquematica la secuencia 
        de operaciones y la correspondiente traza que se sugiere */
 
